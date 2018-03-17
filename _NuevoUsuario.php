0ad3af603b57a000f1d2a9315369a798511fcc71
Marko1496 <mrm.96@hotmail.com>
2018-03-17 15:34:46 -0600

    Validacion de creacion de usuarios

--- _NuevoUsuario.php
+++ _NuevoUsuario.php
@@ -1,23 +1,44 @@
 <?php
+function VerificaUsuario($nombreUsuario){
+	$ruta = "usuarios.txt";
+	if(file_exists($ruta)){
+		$datos = file_get_contents($ruta);
+		$array = explode("#", $datos);
+		foreach ($array as $key => $value) {
+			$arrayDatos[$key] = explode("@", $value);
+			if($arrayDatos[$key][0] == $nombreUsuario){
+				return TRUE;
+			}
+		}
+		return FALSE;
+	}
+	else{
+		return FALSE;
+	}
+}
+
 if((isset($_POST['nombre_nuevo']) && !empty($_POST['nombre_nuevo'])) && 
 	(isset($_POST['contrasena_nuevo']) && !empty($_POST['contrasena_nuevo'])) && 
 	isset($_POST['submit_nuevo'])){
-	$ruta = "usuarios.txt";
-	$nombreU = $_POST['nombre_nuevo'];
-	$contrasenaU = $_POST['contrasena_nuevo'];
-	$archivo = fopen($ruta, 'a+');
-	$registro = $nombreU."@".$contrasenaU."#";
-	fwrite($archivo, $registro);
-	fclose($archivo);
-	mkdir($nombreU);
-	$indice = fopen($nombreU."/indice.txt", 'a+');
-	fclose($indice);
-	$datos = fopen($nombreU."/datos.txt", 'a+');
-	fclose($datos);
+
+	if(!VerificaUsuario($_POST['nombre_nuevo'])){
+		$ruta = "usuarios.txt";
+		$nombreU = $_POST['nombre_nuevo'];
+		$contrasenaU = $_POST['contrasena_nuevo'];
+		$archivo = fopen($ruta, 'a+');
+		$registro = $nombreU."@".$contrasenaU."#";
+		fwrite($archivo, $registro);
+		fclose($archivo);
+		mkdir($nombreU);
+		$indice = fopen($nombreU."/indice.txt", 'a+');
+		fclose($indice);
+		$datos = fopen($nombreU."/datos.txt", 'a+');
+		fclose($datos);
+	}
 }
 ?>
 <html>
 	<head>
-		<meta http-equiv="refresh" content="0; url=NuevoUsuario.php">
+		<meta http-equiv="refresh" content="0; url=login.php">
 	</head>
 </html>
