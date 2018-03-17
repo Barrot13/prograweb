4eddf3e595a8867e747e1e85829db05551abe3fe
Marko1496 <mrm.96@hotmail.com>
2018-03-17 16:04:31 -0600

    Validacion Login

--- _login.php
+++ _login.php
@@ -1,9 +1,52 @@
 <?php
+function VerificaInicio($nombre, $contrasena){
+	$ruta = "usuarios.txt";
+	if(file_exists($ruta)){
+		$datos = file_get_contents($ruta);
+		$array = explode("#", $datos);
+		foreach ($array as $key => $value) {
+			$arrayDatos[$key] = explode("@", $value);
+			if($arrayDatos[$key][0] == $nombre && $arrayDatos[$key][1] == $contrasena){
+				return TRUE;
+			}
+		}
+		return FALSE;
+	}
+	else{
+		return FALSE;
+	}
+}
 
+if((isset($_POST['nombre_login']) && !empty($_POST['nombre_login'])) && 
+	(isset($_POST['contrasena_login']) && !empty($_POST['contrasena_login'])) && 
+	isset($_POST['submit_login'])){
+
+	if(VerificaInicio($_POST['nombre_login'], $_POST['contrasena_login'])){
+		?>
+		<html>
+			<head>
+				<meta http-equiv="refresh" content="0; url=principal.php">
+			</head>
+		</html>
+		<?php
+	}
+	else{
+		?>
+		<html>
+			<head>
+				<meta http-equiv="refresh" content="0; url=login.php">
+			</head>
+		</html>
+		<?php
+	}
+}
+else{
+	?>
+	<html>
+		<head>
+			<meta http-equiv="refresh" content="0; url=login.php">
+		</head>
+	</html>
+	<?php
+}
 ?>
-
-<html>
-	<head>
-		<meta http-equiv="refresh" content="0; url=principal.php">
-	</head>
-</html>
