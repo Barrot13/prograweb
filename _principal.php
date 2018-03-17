2e366e4501e4cac908e403a1cb59e495646a898f
Marko1496 <mrm.96@hotmail.com>
2018-03-17 17:45:20 -0600

    Subir archivos epub

--- _principal.php
+++ _principal.php
@@ -8,7 +8,7 @@ $nombre_archivo = $_FILES['userfile']['name'];
 $tipo_archivo = $_FILES['userfile']['type'];
 $tamano_archivo = $_FILES['userfile']['size'];
 //compruebo si las características del archivo son las que deseo
-if (!((strpos($tipo_archivo, "docx") || strpos($tipo_archivo, "pdf")) && ($tamano_archivo < 1000000))) {
+if (strpos($tipo_archivo, "epub") === false && $tamano_archivo > 100000) {
     echo "La extensión o el tamaño de los archivos no es correcta: $tipo_archivo. <br><br><table><tr><td><li>Se permiten archivos .gif o .jpg<br><li>se permiten archivos de 100 Kb máximo: $tamano_archivo.</td></tr></table>";
 }else{
     if (move_uploaded_file($_FILES['userfile']['tmp_name'], $nombre_archivo)){
